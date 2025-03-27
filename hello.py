fb2270f0ce812d05a01129e08a311bac0266ccc1
*hello* <mateo.moligon@gmail.com>
2025-03-27 08:13:55 -0500

    commit

--- hello.py
+++ hello.py
@@ -1,15 +1,26 @@
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
+from kivy.uix.boxlayout import BoxLayout
 
-class HelloApp(App):
+class Saludo(App):
+
     def build(self):
-        nombre = 'David'
-        label = Label(
-            text=f"Hola {nombre}",
-            font_size=50,
-            color=('#75ff33')
-            )
-        return label
+        mi_contenedor=BoxLayout()
+        mi_contenedor.orientation ="vertical"
+        mi_boton = Button( text="Saludar" )
+        self.mi_texto = TextInput()
+        self.mi_etiqueta = Label( text="Hola Mundo")
+        mi_contenedor.add_widget(self.mi_texto)
+        mi_contenedor.add_widget(mi_boton)
+        mi_boton.bind(on_press=self.saludar)
+        mi_contenedor.add_widget(self.mi_etiqueta)
+        return mi_contenedor
 
-if __name__ == "__main__":
-    HelloApp().run()
+    def saludar(self, sender):
+        self.mi_etiqueta.text = "Hola "+ self.mi_texto.text
+        print("Has llamado al metodo saludar!")
+
+miaplicacion = Saludo()
+miaplicacion.run()
